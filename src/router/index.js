3b3952bff8ccbaf0577fd9dcff45e5d23be66eda
Josh Medeski <josh.medeski@gmail.com>
2018-11-03 15:58:15 -0500

    Redo Vue
    - Replace entire Vue application with other project
    - Add firebase functionality
    - Add login page
    - Add basic "main" page when logged in

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,51 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Main from '@/components/Main'
+import Vue from "vue";
+import Router from "vue-router";
 
-Vue.use(Router)
+import Main from "@/components/Main";
+import Login from "@/components/Login";
+import SignUp from "@/components/SignUp";
+import firebase from "firebase";
 
-export default new Router({
+Vue.use(Router);
+
+let router = new Router({
   routes: [
     {
-      path: '/',
-      name: 'Main',
-      component: Main
+      path: "*",
+      redirect: "/login"
+    },
+    {
+      path: "/",
+      redirect: "/login"
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login
+    },
+    {
+      path: "/sign-up",
+      name: "SignUp",
+      component: SignUp
+    },
+    {
+      path: "/main",
+      name: "Main",
+      component: Main,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
-})
+});
+
+router.beforeEach((to, from, next) => {
+  let currentUser = firebase.auth().currentUser;
+  let requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+
+  if (requiresAuth && !currentUser) next("login");
+  else if (!requiresAuth && currentUser) next("main");
+  else next();
+});
+
+export default router;
